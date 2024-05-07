104adc2f9e6deec4271f0be5c35dbad95a9187c1
qorditl1248 <qorditl1248@naver.com>
2024-05-07 18:09:02 +0900

    새로운 업로드 파일

---
+++ c_JavaScript/0503/callback01.js
@@ -0,0 +1,20 @@
+// map 사용 
+// : 주어진 숫자 배열의 각 요소에 10를 더한 새로운 배열을 생생 
+
+const numbers = [1, 2, 3, 4, 5];
+// const addedTen 
+
+// filter 사용 
+// : 주어진 숫자 배열에서 홀수만 추출하여 새로운 배열을 생성 
+
+const numbers2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// const odds 
+
+
+// map과 filter 함께 사용 
+// : 주어진 숫자 배열에서 짝수만 찾아 각 숫자에 5를 곱한 새로운 배열을 생성 
+
+const numbers3 = [1, 2, 3, 4, 5, 6];
+
+// const evenTimesFive 
